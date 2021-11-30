c2f8329f8cb2ea8376ed381920d73a9b309176ea
buncis <buncismamen@gmail.com>
2021-11-30 20:32:36 +0700

    tambahin filter team
    Now we can start to see the benefit of the work we did in the last section.
    We can add filtering by a new option with just a few simple changes to the model.
    
    We updated the model to add team_id to the list of valid FILTER_PARAMS,
    added the by_team scope, and then added by_team to the filters method.
    
    No need to change our controller or touch the Filterable module —
    updating the model is all we need.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,13 +1,15 @@
 class Player < ApplicationRecord
   belongs_to :team
 
-  FILTER_PARAMS = %i[name column direction].freeze
+  FILTER_PARAMS = %i[name team_id column direction].freeze
 
   scope :by_name, ->(query) { where('players.name ilike ?', "%#{query}%") }
+  scope :by_team, ->(team_id) { where(team_id: team_id) if team_id.present? }
 
   def self.filter(filters)
     Player.includes(:team)
           .by_name(filters['name'])
+          .by_team(filters['team_id'])
           .order("#{filters['column']} #{filters['direction']}")
   end
 end
